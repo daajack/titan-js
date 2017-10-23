14e10d054697e4e688e6c520663608000130c2a6
Rodolphe Gerber <rodolphe@lemon.ch>
2017-10-23 17:43:05 +0200

    add App add multiple views add compile callback add dom.Node fix dom types add dom.Element.read() add fs.Main add view.Import

--- browser.js
+++ browser.js
@@ -6,12 +6,14 @@ window.titan = {};
 
 (function(container) {
 
+  let App = require('./lib/App.js');
   let Compiler = require('./lib/compiler/Compiler.js');
   
   let View = require('./public/View.js');
 //  let Query = require('./lib/view/public/View.js');
   
   
+  container.App = App;
   container.Compiler = Compiler;
   container.View = View;
   
--- main.js
+++ main.js
@@ -4,25 +4,32 @@ global.__base = __dirname + '/lib';
 
 (function() {
 
+  let App = require('./lib/App.js');
+  let titan = new App;
+  
   let Compiler = require('./lib/compiler/Compiler.js');
   let Document = require('./lib/dom/Document.js');
-  let fs = require('fs');
+  
+  let fs = titan.fs;
 
   let compiler = new Compiler();
 
-  fs.readFile('test/index.vml', 'utf8', function(err, datas)
+  fs.getFile('test/index.vml').open(function(datas)
   {
     let DOMParser = require('xmldom').DOMParser;
     
     let document = new Document(new DOMParser);
     document.parse(datas);
     
-    compiler.prepare(document, 0, (result) => {
+//    let views = [];
+    
+    compiler.prepare(document, 0, (view, result) => {
+//      views.push.apply(view.views);
       result.map(w => {
+        console.log('+++' + view.name + '+++');
         console.log('[[' + w.name + ']]');
         console.log(w.content);
       });
     });
   });
 })()
-
